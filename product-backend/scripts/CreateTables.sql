97e8499dd0fe9c3887bac162ca2b0aba72e996c1
JanOstrowski1 <jan.ostrowski1337@gmail.com>
2023-10-06 13:24:17 +0200

    feat: added Bands and Capabilities scripts to database

--- product-backend/scripts/CreateTables.sql
+++ product-backend/scripts/CreateTables.sql
@@ -11,18 +11,32 @@ roleId TINYINT,
 FOREIGN KEY (roleId) REFERENCES UserRoles (id)
 );
 
-CREATE TABLE IF NOT EXISTS JobRoles(
+CREATE TABLE IF NOT EXISTS Bands (
+id SMALLINT PRIMARY KEY AUTO_INCREMENT,
+name varchar(50) UNIQUE NOT NULL,
+level TINYINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Capabilities (
+    id smallint NOT NULL AUTO_INCREMENT,
+    capabilityName varchar(50),
+    leadName varchar(50),
+    capabilityLeadPicture LONGTEXT,
+    message varchar(3000),
+    PRIMARY KEY(id)
+    );
+
+CREATE TABLE IF NOT EXISTS JobRoles (
 id SMALLINT PRIMARY KEY AUTO_INCREMENT,
 name varchar(50) UNIQUE,
 description varchar(3000),
-link varchar(1000)
+link varchar(1000),
+bandId SMALLINT,
+capabilityId SMALLINT,
+FOREIGN KEY (bandId) REFERENCES Bands (id),
+FOREIGN KEY (capabilityId) REFERENCES Capabilities (id)
 );
 
-CREATE TABLE Token (
-id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-email varchar(50) NOT NULL,
-token varchar(64) NOT NULL,
-expiry DATETIME NOT NULL,
-FOREIGN KEY (email) REFERENCES Users(email)
-);
+ 
+
 
