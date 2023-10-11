d08c1c60eb6b5238d8f33cedb94cad16590e9e32
Dominik Suszek <73228738+dsuszek@users.noreply.github.com>
2023-10-11 12:25:02 +0200

    fix: classes inside Loggers corrected
    Co-authored-by: Lukasz Jercha <ljercha@users.noreply.github.com>

--- product-backend/src/main/java/org/kainos/ea/db/DatabaseConnector.java
+++ product-backend/src/main/java/org/kainos/ea/db/DatabaseConnector.java
@@ -9,7 +9,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DatabaseConnector {
-    private final static Logger logger = LoggerFactory.getLogger(JobRoleService.class);
+    private final static Logger logger = LoggerFactory.getLogger(DatabaseConnector.class);
     private static Connection conn;
 
     public static Connection getConnection() throws SQLException {
