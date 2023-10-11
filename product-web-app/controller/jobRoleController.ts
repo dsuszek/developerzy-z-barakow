b6f48264548a070168f417b8dd549b0ce7d431ff
Wojciech Mierzejewski <mierzvoj@gmail.com>
2023-10-11 21:02:19 +0200

    Add field select capability to add-job-roles form

--- product-web-app/controller/jobRoleController.ts
+++ product-web-app/controller/jobRoleController.ts
@@ -4,13 +4,26 @@ import JobRoleService from '../service/jobRoleService.js';
 import JobRole from '../model/jobRole.js';
 import JobRoleValidator from '../service/jobRoleValidator.js';
 import logger from '../service/logger.js';
+import Capability from '../model/capability.js';
+import CapabilityService from '../service/capabilityService.js';
+import CapabilityValidator from '../service/capabilityValidator.js';
 
 export default class JobRoleController {
   private jobRoleService = new JobRoleService(new JobRoleValidator());
 
+  private capabilityService = new CapabilityService(new CapabilityValidator());
+
   appRoutes(app: Application) {
     app.get('/admin/add-job-roles', async (req: Request, res: Response) => {
-      res.render('add-job-roles');
+      let data: Capability[] = [];
+      try {
+        data = await this.capabilityService.getCapabilities();
+      } catch (e) {
+        console.error(e);
+      }
+      res.render('add-job-roles', {
+        capabilities: data,
+      });
     });
 
     app.post('/admin/add-job-roles', async (req: Request, res: Response) => {
