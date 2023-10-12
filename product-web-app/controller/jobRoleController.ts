9406c04a2d67e99a4e6151e1d40cc1ddcb13d7a6
Wojciech Mierzejewski <mierzvoj@gmail.com>
2023-10-12 11:40:02 +0200

    Add multer fix

--- product-web-app/controller/jobRoleController.ts
+++ product-web-app/controller/jobRoleController.ts
@@ -30,6 +30,7 @@ export default class JobRoleController {
       const data: JobRole = req.body;
       data.name = sanitize(data.name).trim();
       data.description = sanitize(data.description).trim();
+      data.link = sanitize(data.link);
 
       try {
         const newJobRole = await this.jobRoleService.createJobRole(data);
