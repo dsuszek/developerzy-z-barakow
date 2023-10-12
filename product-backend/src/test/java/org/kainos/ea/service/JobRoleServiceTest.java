33e19535f363a00d9c7ec7f1101699d6984597aa
Dominik Suszek <dominik.suszek@gmail.com>
2023-10-12 10:48:45 +0200

    feat: delete button for /job-roles/{id} view added

--- product-backend/src/test/java/org/kainos/ea/service/JobRoleServiceTest.java
+++ product-backend/src/test/java/org/kainos/ea/service/JobRoleServiceTest.java
@@ -12,12 +12,9 @@ import org.kainos.ea.model.JobRoleRequest;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.SQLException;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,7 +27,8 @@ public class JobRoleServiceTest {
             "Technology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology LeaderTechnology Leader",
             "A technology leader is key strategic role within the business making executive technology decisions on behalf of the business, based upon the sector and practices’ strategic direction and goals. The core responsibilities of a technology leader in Kainos include setting a Technology direction, a technical advisor to the business and C-level clients, maintaining a commercial edge over other technology services providers, developing and nurturing technical talent across the organisation and representing Kainos as a technology evangelist.",
             "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20Profile%20%2D%20Technology%20Leader%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1",
-            (short) 1
+            (short) 1,
+            (short) 2
     );
 
 
@@ -43,7 +41,8 @@ public class JobRoleServiceTest {
                         "The projects you will join are varied, and often highly visible. You will be working in fast- paced, agile environments, so it is important for you to make sound, reasoned decisions, and recommendations on front-end and user interfaces with your colleagues.\n" +
                         "You are determined, flexible and always constructive; proactive in improving things and are always inclusive and respectful in your interactions with your team. You will be working alongside talented, diverse, enthusiastic colleagues, who will help you learn and develop as you, in turn, mentor those around you.",
                 "https://kainossoftwareltd.sharepoint.com/sites/PeopleTeam-SharedDrive/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FPeopleTeam%2DSharedDrive%2FShared%20Documents%2FPeople%20Team%20Shared%20Drive%2FOrganisational%20Development%20%26%20Learning%2FCareer%20Lattice%2FApproved%20Job%20Profiles%2FEngineering%2FEngineering%2FJob%20Profile%20%2D%20Front%2DEnd%20Engineer%20%28A%29%2Epdf&parent=%2Fsites%2FPeopleTeam%2DSharedDrive%2FShared%20Documents%2FPeople%20Team%20Shared%20Drive%2FOrganisational%20Development%20%26%20Learning%2FCareer%20Lattice%2FApproved%20Job%20Profiles%2FEngineering%2FEngineering&p=true&ga=1",
-                (short) 1);
+                (short) 1,
+                (short) 2);
 
         String mockedValidationError = "Invalid job role exception";
 
@@ -58,7 +57,11 @@ public class JobRoleServiceTest {
     @Test
     void createJobRole_When_ThereIsDatabaseError_Expect_FailedToCreateJobRoleExceptionToBeThrown() throws SQLException, FailedToCreateJobRoleException {
         // given
-        JobRoleRequest mockedJobRoleRequest = new JobRoleRequest("Front-end Engineer", "As a Front-end Engineer in Kainos, you will have the opportunity to use your expertise in developing high quality user interface solutions which delight our customers and impact the lives of users worldwide.", "https://kainossoftwareltd.sharepoint.com/sites/PeopleTeam-SharedDrive/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FPeopleTeam%2DSharedDrive%2FShared%20Documents%2FPeople%20Team%20Shared%20Drive%2FOrganisational%20Development%20%26%20Learning%2FCareer%20Lattice%2FApproved%20Job%20Profiles%2FEngineering%2FEngineering%2FJob%20Profile%20%2D%20Front%2DEnd%20Engineer%20%28A%29%2Epdf&parent=%2Fsites%2FPeopleTeam%2DSharedDrive%2FShared%20Documents%2FPeople%20Team%20Shared%20Drive%2FOrganisational%20Development%20%26%20Learning%2FCareer%20Lattice%2FApproved%20Job%20Profiles%2FEngineering%2FEngineering&p=true&ga=1", (short) 1);
+        JobRoleRequest mockedJobRoleRequest = new JobRoleRequest(
+                "Front-end Engineer",
+                "As a Front-end Engineer in Kainos, you will have the opportunity to use your expertise in developing high quality user interface solutions which delight our customers and impact the lives of users worldwide.", "https://kainossoftwareltd.sharepoint.com/sites/PeopleTeam-SharedDrive/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FPeopleTeam%2DSharedDrive%2FShared%20Documents%2FPeople%20Team%20Shared%20Drive%2FOrganisational%20Development%20%26%20Learning%2FCareer%20Lattice%2FApproved%20Job%20Profiles%2FEngineering%2FEngineering%2FJob%20Profile%20%2D%20Front%2DEnd%20Engineer%20%28A%29%2Epdf&parent=%2Fsites%2FPeopleTeam%2DSharedDrive%2FShared%20Documents%2FPeople%20Team%20Shared%20Drive%2FOrganisational%20Development%20%26%20Learning%2FCareer%20Lattice%2FApproved%20Job%20Profiles%2FEngineering%2FEngineering&p=true&ga=1",
+                (short) 1,
+                (short) 2);
         // when
         when(jobRoleDaoMock.createJobRole(mockedJobRoleRequest)).thenThrow(new SQLException());
         // then
@@ -71,7 +74,12 @@ public class JobRoleServiceTest {
     @Test
     void createJobRole_When_JobRoleInputIsValid_Expect_NewJobRoleToBeReturned() throws SQLException, FailedToCreateJobRoleException, InvalidJobRoleException {
         // given
-        JobRoleRequest mockedJobRoleRequest = new JobRoleRequest("MockedName", "MockedDescription", "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test", (short) 1);
+        JobRoleRequest mockedJobRoleRequest = new JobRoleRequest(
+                "MockedName",
+                "MockedDescription",
+                "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test",
+                (short) 1,
+                (short) 2);
         // when
         when(jobRoleDaoMock.createJobRole(mockedJobRoleRequest)).thenReturn(MOCKED_JOB_ROLE);
         // then
